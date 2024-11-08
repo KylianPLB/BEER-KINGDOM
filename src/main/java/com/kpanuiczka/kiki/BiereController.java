1c65dca7ce971fce9c19fab8864ea6c32e68bafc
El Kikiii <151017130+KylianPLB@users.noreply.github.com>
2024-11-08 13:30:59 +0100

    test

--- src/main/java/com/kpanuiczka/kiki/BiereController.java
+++ src/main/java/com/kpanuiczka/kiki/BiereController.java
@@ -1,23 +1,17 @@
 package com.kpanuiczka.kiki;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kpanuiczka.kiki.entities.Biere;
 import com.kpanuiczka.kiki.service.BieresService;
-
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-// import com.kpanuiczka.kiki.entities.Biere;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -53,7 +47,6 @@ public class BiereController {
     public void modifierBiere(@PathVariable Long id, @RequestBody Biere biereJSON) {
         bieresService.modifierBiere(id, biereJSON);
     }
-    
     
     
 
